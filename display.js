1b2efd6abaaef0ce5237d960d40813f520c74064
kaimorris <kaimorrislove7@gmail.com>
2018-05-27 18:54:12 -0700

    Create display.js

---
+++ display.js
@@ -0,0 +1,24 @@
+function showText(e){
+	document.getElementById("text").style.display = "none";
+}
+setTimeout(showText, 30000);
+
+function myFunction() {
+  document.getElementById("demo").innerHTML = "WRONG!";
+}
+
+function myFunction1() {
+  document.getElementById("demo2").innerHTML = "WRONG!";
+}
+
+function myFunction2() {
+  document.getElementById("demo3").innerHTML = "CORRECT!";
+}
+
+function myFunction3() {
+  document.getElementById("demo4").innerHTML = "WRONG!";
+}
+
+function myfunction4(){
+	var x = document.getElementById("myAudio").autoplay();
+}
